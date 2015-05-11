233e5228e1b133f9e20d2eb3037fcf1c8e99a574
Rohit Grover <rohit.grover@arm.com>
2015-05-11 12:01:59 +0100

    minor white space diffs.

--- nRF51Gap.h
+++ nRF51Gap.h
@@ -82,8 +82,8 @@ public:
 
             ble_gap_scan_params_t scanParams = {
                 .active      = scanningParams.getActiveScanning(), /**< If 1, perform active scanning (scan requests). */
-                .selective   = 0,        /**< If 1, ignore unknown devices (non whitelisted). */
-                .p_whitelist = NULL,     /**< Pointer to whitelist, NULL if none is given. */
+                .selective   = 0,    /**< If 1, ignore unknown devices (non whitelisted). */
+                .p_whitelist = NULL, /**< Pointer to whitelist, NULL if none is given. */
                 .interval    = scanningParams.getInterval(),  /**< Scan interval between 0x0004 and 0x4000 in 0.625ms units (2.5ms to 10.24s). */
                 .window      = scanningParams.getWindow(),    /**< Scan window between 0x0004 and 0x4000 in 0.625ms units (2.5ms to 10.24s). */
                 .timeout     = scanningParams.getTimeout(),   /**< Scan timeout between 0x0001 and 0xFFFF in seconds, 0x0000 disables timeout. */
