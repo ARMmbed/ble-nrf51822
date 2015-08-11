1da1aa8622850e3b6c4deab5eeba99cb9fe1b725
Rohit Grover <rohit.grover@arm.com>
2015-08-11 14:42:53 +0100

    fix #39: have a single call to SOFTDEVICE_HANDLER_INIT() to remove duplication of BLE_EVT_BUFFER.

--- source/btle/btle.cpp
+++ source/btle/btle.cpp
@@ -52,11 +52,13 @@ static void sys_evt_dispatch(uint32_t sys_evt)
 
 error_t btle_init(void)
 {
+    nrf_clock_lfclksrc_t clockSource;
     if (NRF_CLOCK->LFCLKSRC & (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos)) {
-        SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, NULL);
+        clockSource = NRF_CLOCK_LFCLKSRC_XTAL_20_PPM;
     } else {
-        SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_RC_250_PPM_4000MS_CALIBRATION, NULL);
+        clockSource = NRF_CLOCK_LFCLKSRC_RC_250_PPM_4000MS_CALIBRATION;
     }
+    SOFTDEVICE_HANDLER_INIT(clockSource, NULL);
 
     // Enable BLE stack
     /**
